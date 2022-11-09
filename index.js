8afd9da15d14bc37c1b58b366c1620f93ca31423
Ahmad Musa <amaahmadmusa@gmail.com>
2022-11-09 12:04:33 -0800

    all services api is created

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.limit(3).toArray();
+      console.log(services);
       res.send(services);
     });
   } finally {
