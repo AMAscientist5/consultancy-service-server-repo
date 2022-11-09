45e2dd1ccf1cf8f147812b67775d3e4519414935
Ahmad Musa <amaahmadmusa@gmail.com>
2022-11-09 09:29:16 -0800

    env file is added

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://dbUser4:<password>@cluster0.mkpcxwu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("The server is running");
