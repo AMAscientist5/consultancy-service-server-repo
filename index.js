8eb712e7fd8c0b87a06b370b466235aea5a36bc0
Ahmad Musa <amaahmadmusa@gmail.com>
2022-11-10 06:58:52 -0800

    reviews get api is added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mkpcxwu.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -22,13 +21,48 @@ async function run() {
     const serviceCollection = client
       .db("aradunVisaConsultancy")
       .collection("services");
+    const reviews = client.db("aradunVisaConsultancy").collection("reviews");
 
     app.get("/limit-services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.limit(3).toArray();
-      console.log(services);
       res.send(services);
+    });
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(req.params);
+      const query = { _id: ObjectId(id) };
+      // const query = { ServiceName: "Student Visa" };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviews.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await reviews.findOne(query);
+      res.send(service);
     });
   } finally {
   }
